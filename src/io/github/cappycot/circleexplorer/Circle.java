a01780a86b68683f31bc37f80ecdef106c48185a
Chris Wang <chriswang444@gmail.com>
2015-10-10 18:41:19 -0500

    Initial creation.

---
+++ src/io/github/cappycot/circleexplorer/Circle.java
@@ -0,0 +1,35 @@
+package io.github.cappycot.circleexplorer;
+
+/**
+ * Object holding x and y coordinates.
+ * 
+ * @author Chris Wang
+ */
+public class Circle {
+	/* Instance Variables */
+	private double x;
+	private double y;
+
+	/* Getters */
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	/* Setters */
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	/* Operations */
+	public double distFrom(double x, double y) {
+		return Math.sqrt((Math.pow(this.x - x, 2) + Math.pow(this.y - y, 2)));
+	}
+}
