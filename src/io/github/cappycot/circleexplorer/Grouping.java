1744d8b71294f7a58138b387c60fc452c8e060b3
Chris Wang <chriswang444@gmail.com>
2015-10-10 18:32:20 -0500

    Math operations.

--- src/io/github/cappycot/circleexplorer/Grouping.java
+++ src/io/github/cappycot/circleexplorer/Grouping.java
@@ -1,10 +1,11 @@
 package io.github.cappycot.circleexplorer;
 
 public class Grouping {
-
+	/* Instance Variables */
 	private int size = 1;
 	private int cluster = 1;
 
+	/* Constructors */
 	public Grouping(int size, int cluster) {
 		this.size = size;
 		this.cluster = cluster;
@@ -15,6 +16,7 @@ public class Grouping {
 		this.cluster = group.cluster;
 	}
 
+	/* Getters */
 	public int getSize() {
 		return size;
 	}
@@ -22,25 +24,72 @@ public class Grouping {
 	public int getCluster() {
 		return cluster;
 	}
-	
+
 	public int getTotal() {
 		return size * cluster;
 	}
 
-	public void incSize(int inc) {
+	/* Setters */
+	public Grouping incSize(int inc) {
 		size += inc;
 		if (size < 1)
 			size = 1;
+		return this;
 	}
 
-	public void incCluster(int inc) {
+	public Grouping incCluster(int inc) {
 		cluster += inc;
 		if (cluster < 1)
 			cluster = 1;
+		return this;
 	}
 
-	public void conform(Grouping group) {
+	public Grouping conform(Grouping group) {
 		this.size = group.size;
 		this.cluster = group.cluster;
+		return this;
+	}
+
+	/* Operations */
+	public Grouping rotate() {
+		int p = this.size;
+		size = cluster;
+		cluster = p;
+		return this;
+	}
+
+	/**
+	 * Multiplies the cluster of this Grouping by another Grouping.
+	 * 
+	 * @param group
+	 */
+	public Grouping merge(Grouping group) {
+		this.cluster += group.getTotal();
+		return this;
+	}
+
+	/**
+	 * Attempts to append the units from another Grouping to this Grouping
+	 * divided by a common cluster.
+	 * 
+	 * @param group
+	 * @return remainder
+	 */
+	public Grouping append(Grouping group) {
+		if (this.cluster > group.getTotal()) {
+			return group;
+		} else if (this.cluster == group.cluster) {
+			this.size += group.size;
+			return null;
+		}
+		group.rotate();
+		if (this.cluster == group.cluster) {
+			this.size += group.size;
+			return null;
+		}
+		
+		this.size += group.getTotal() / this.cluster;
+		group = new Grouping(1, group.getTotal() % this.cluster);
+		return group;
 	}
 }
